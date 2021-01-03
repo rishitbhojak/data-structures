ac86690fd70f802edac56011a832e22eaf65c5a9
Rishit Bhojak <rajabhojak@gmail.com>
2021-01-03 21:14:41 +0530

    refactor : Arrow function for pointers

--- arrays-1.C
+++ arrays-1.C
@@ -1,11 +1,25 @@
 #include<stdio.h>
+#include<stdlib.h>
 struct myArray 
 {
     int total_size;
     int used_size;
-    int *ptr;
+    int * ptr;
 };
+void createArray(struct myArray * a, int tSize, int uSize)
+{
+    // (*a).total_size=tSize;
+    // (*a).used_size=uSize;
+    // (*a).ptr = (int *) malloc(tSize*sizeof(int));
+
+    //the above written code is similar to the one below
+    a->total_size = tSize;
+    a->used_size = uSize;
+    a->ptr = (int *)malloc(tSize*sizeof(int));
+}
 int main()
 {
+    struct myArray marks;
+    createArray(&marks, 100,20);
     return 0;
 }
