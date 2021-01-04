bc7d942dd55844d2ab3677c01b71df98ddfb9dd2
Rishit Bhojak <rajabhojak@gmail.com>
2021-01-04 16:04:44 +0530

    feat : Insertion operation

--- arrays-2.C
+++ arrays-2.C
@@ -7,12 +7,30 @@ void display(int arr[], int n)
     {
         printf("%d\n", arr[i]);
     }
+    printf("\n");
 }
-
+int indInsertion(int arr[],int size ,int element,int capacity,int index)
+{
+    //Insertion
+    if(size>=capacity)
+    {
+        return -1;
+    }
+    for(int i=size-1; i>=index; i--)
+    {
+        arr[i+1] = arr[i]; // shifting indices by one place
+    }
+    arr[index] = element;
+    return 1;
+}
 int main()
 {
-    int arr[100] = {1,2,6,78};
-    int size=4;
-    display(arr,4);
+    int arr[100] = {7,8,12,27,88};
+    int size=5, element =45, index=3;  //size of array =5
+                                        //element to be inserted = 45 at index=3
+    display(arr,size);
+    indInsertion(arr,size, element, 100, index);
+    size+=1;
+    display(arr, size);
     return 0;
 }
