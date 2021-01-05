a6dd40050d03951c17b3a88d04da5d3f38dd223a
Rishit Bhojak <rajabhojak@gmail.com>
2021-01-05 19:31:52 +0530

    feat : Deletion method added

--- arrays-3.c
+++ arrays-3.c
@@ -13,19 +13,19 @@ void indDeletion(int arr[],int size,int index)
 {
     //Deletion
   
-    for(int i=index; i<size; i++)
+    for(int i=index; i<size-1; i++)
     {
-        arr[i] = arr[i+1]; // shifting indices by one place
+        arr[i] = arr[i+1]; // shifting indices by one place i.e assigning value of right element to the left one
     }
  
 }
 int main()
 {
     int arr[100] = {7,8,12,27,88};
-    int size=5, element =45, index=3;  //size of array =5
-                                        //element to be inserted = 45 at index=3
+    int size=5, index=1;  //size of array =5
+                                        //element to be deleted = 8 at index=3
     display(arr,size);
-    indDeletion(arr,size, element, 100, index);
+    indDeletion(arr,size,index);
     size-=1;
     display(arr, size);
     return 0;
