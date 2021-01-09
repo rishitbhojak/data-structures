12b374285426d13ef8d01d3ba581077a9e82258e
Rishit Bhojak <rajabhojak@gmail.com>
2021-01-09 15:29:13 +0530

    Binary Search

--- searching.c
+++ searching.c
@@ -16,16 +16,34 @@ int linearSearch(int arr[], int size, int element)
 
 int binarySearch(int arr[], int size, int element)
 {
-   int mid,high,low;
-   mid = (low+high)/2;
+   int mid,high=size-1,low=0;
+   while (low<=high)
+   {
+      mid = (low+high)/2;
+   if (arr[mid]==element)
+   {
+      return mid;
+   }
+   if (arr[mid]<element)
+   {
+      low = mid+1;
+   }
+   else
+   {
+      high = mid-1;
+   }
+   }
+   return -1;   
 }
 
 int main()
 {
-    int arr[] = {1,3,5,56,4,3,23,5,4,54634,56,34};
+   //  int arr[] = {1,3,5,56,4,3,23,5,4,54634,56,34}; unsorted array for linear search
+    int arr[] = {1,3,5,56,64,73,123,225,444,54634};
     int size = sizeof(arr)/sizeof(int);
-    int element=54;
-    int searchIndex = linearSearch(arr , size ,element);
+    int element=56;
+   //  int searchIndex = linearSearch(arr , size ,element);
+   int searchIndex = binarySearch(arr , size ,element);
     printf("The element %d was found at index = %d \n", element, searchIndex);
     return 0;
 }
