790032f85e4ac8803ee811728b33c2603d1ce04d
Rishit Bhojak <rajabhojak@gmail.com>
2021-01-10 15:24:52 +0530

    feat : LinkedList Traversal

--- linkedlist.c
+++ linkedlist.c
@@ -1,12 +1,22 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct Node{
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
     int data;
     struct Node *next;
 };
+
+void LinkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d", ptr->data);
+        ptr = ptr->next;
+    }
+}
 int main()
 {
-    struct Node *head ;
+    struct Node *head;
     struct Node *second;
     struct Node *third;
 
@@ -14,17 +24,18 @@ int main()
     head = (struct Node *)malloc(sizeof(struct Node));
     second = (struct Node *)malloc(sizeof(struct Node));
     third = (struct Node *)malloc(sizeof(struct Node));
-    
+
     //Linking 1st and 2nd Node
-    head->data=7;
-    head->next=second;
+    head->data = 7;
+    head->next = second;
 
     //Link third and Second Node
-    second->data=10;
-    second->next=third;
+    second->data = 10;
+    second->next = third;
 
     //Terminate the list at the end
-    third->data=12;
-    third->next=NULL;
+    third->data = 12;
+    third->next = NULL;
+    LinkedListTraversal(head);
     return 0;
 }
