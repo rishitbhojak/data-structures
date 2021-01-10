7725dddcabe2f70aa5837fd77a5880a869427b9c
Rishit Bhojak <rajabhojak@gmail.com>
2021-01-10 15:08:12 +0530

    Linking Nodes and Terminating

--- linkedlist.c
+++ linkedlist.c
@@ -14,5 +14,17 @@ int main()
     head = (struct Node *)malloc(sizeof(struct Node));
     second = (struct Node *)malloc(sizeof(struct Node));
     third = (struct Node *)malloc(sizeof(struct Node));
+    
+    //Linking 1st and 2nd Node
+    head->data=7;
+    head->next=second;
+
+    //Link third and Second Node
+    second->data=10;
+    second->next=third;
+
+    //Terminate the list at the end
+    third->data=12;
+    third->next=NULL;
     return 0;
 }
