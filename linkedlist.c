1e528bf489f542cc6faebeca3433c58cf917b06a
Rishit Bhojak <rajabhojak@gmail.com>
2021-01-10 14:59:15 +0530

    Heap Memory Allocation

--- linkedlist.c
+++ linkedlist.c
@@ -7,6 +7,12 @@ struct Node{
 int main()
 {
     struct Node *head ;
+    struct Node *second;
+    struct Node *third;
+
+    //Allocate memory for nodes in Heap Memory
     head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
     return 0;
 }
