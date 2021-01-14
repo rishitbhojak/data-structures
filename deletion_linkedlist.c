4c711fd73dd1d8cd79b92996886bf061f81cffc2
Rishit Bhojak <rajabhojak@gmail.com>
2021-01-14 22:18:05 +0530

    feat : deleting an element with a given value in  a list

--- deletion_linkedlist.c
+++ deletion_linkedlist.c
@@ -50,6 +50,23 @@ p->next = NULL;
 free(q);
 return head;
 }
+// Case 4: Deleting an element with a given value from the LinkedList
+struct Node * deleteAtIndex(struct Node * head, int value)
+{
+struct Node *p = head;
+struct Node *q = head->next;
+while(q->data!=value && q->next!=NULL)
+{
+    p=p->next;
+    q=q->next;
+}
+if(q->data==value)
+{
+    p->next = q->next;
+    free(q);
+}
+return head;
+}
 int main()
 {
     struct Node *head;
