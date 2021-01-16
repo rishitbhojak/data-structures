65c6d2b87955d1ba3840c73c8c25d3510c63e35b
Rishit Bhojak <rajabhojak@gmail.com>
2021-01-16 11:33:42 +0530

    created LinkedList

--- circular_Linkedlist.c
+++ circular_Linkedlist.c
@@ -1,6 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+struct Node 
+{
+    int data;
+    struct Node *next;
+};
 int main()
 {
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+
+    //Allocate memory for nodes in Heap Memory
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    //Linking First and Second Node
+    head->data = 4;
+    head->next = second;
+
+    //Link Second and Third Node
+    second->data = 3;
+    second->next = third;
+
+  //Link third and Fourth Node
+    third->data = 6;
+    third->next = fourth;
+
+    //Terminate the list at the end
+    fourth->data = 1;
+    fourth->next = NULL;
+    LinkedListTraversal(head);
     return 0;
 }
